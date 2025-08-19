743ed4164bac64ea40b7ecd2e286f3ad0f9a138c
jorge dantas <91327490+jcfdantas@users.noreply.github.com>
2025-08-19 09:54:43 -0300

    Add files via upload

---
+++ loja de doce/19.08.py
@@ -0,0 +1,16 @@
+doces = []
+preco = []
+
+def menu():
+   print("      Armázem Loja de Doce      ") 
+   print("1) Cadastrar produto") 
+   print("2) Listar produtos cadastrados") 
+   print("3) Atualizar lista de produtos") 
+   print("4) Deletar produto") 
+
+   '''cadastrar, listar, alterar e deletar'''
+
+
+def cadatrar(doces, preco):
+    
+menu()
